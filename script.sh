1c8af6ebaf2fd30c8b766ef811cd783c02d46c26
Himanshu Tank <39885977+himanshutak8@users.noreply.github.com>
2023-05-26 17:49:57 +0530

    Update script.sh
    update

--- script.sh
+++ script.sh
@@ -1,10 +1,4 @@
-function add_fstab_entry () {
-  local FS="${1}"
-  local MOUNT="${2}"
-  local FS_TYPE="${3}"
- local OPTS="${4}"
-  if ! grep -q "${FS}" /etc/fstab ; then
-    echo "${FS} ${MOUNT} ${FS_TYPE} ${OPTS} 0 0" >> /etc/fstab
-  fi
-}
+#!/bin/bash
+echo "fs-018d8b28201f22e35.fsx.ap-south-1.amazonaws.com@tcp:/5ea2hbmv lustre defaults,_netdev,flock,user_xattr,noatime,noauto,x-systemd.automoun 0 0" >> /etc/fstab
+
 
